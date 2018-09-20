b68026c13d09806b70109d9b702930b74a78ee6e
Daniel <dahnguyen@ucdavis.edu>
2018-09-20 00:11:57 -0700

    server-side rendering 3 modules

--- proxy/server.js
+++ proxy/server.js
@@ -1,12 +1,34 @@
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
     console.log(`server running at ${port}`)
 });
+
+//Overview
+
+app.get('/api/restaurants/:restaurantId/overview', function(req, res) {
+    var redirectUrl = `http://localhost:8008${req.url}`;
+    res.redirect(redirectUrl);
+  });
+  
+  
+  //Photos
+  
+  app.get('/api/restaurants/:restaurantId/photos', function(req, res) {
+    var redirectUrl = `http://localhost:3005${req.url}`;
+    res.redirect(redirectUrl);
+  });
+  
+  
+
+  //Reviews
+  
+  app.get('/api/restaurants/:restaurantId/reviews', function(req, res) {
+    var redirectUrl = `http://localhost:1337${req.url}`;
+    res.redirect(redirectUrl);
+  });
